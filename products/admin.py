2927dbcaf73154cd31f3353102c11dc32de5296a
Jonathan Costa <jhoncosta2@gmail.com>
2025-02-16 10:28:06 -0300

    criação tabelas sqlite

---
+++ products/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Brand
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active', 'description', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('is_active',)
